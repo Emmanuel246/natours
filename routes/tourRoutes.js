0f36e5f99407958019a40afc68e67a2a676e6fdf
Emmanuel246 <makanjuolaiseoluwa1@gmail.com>
2025-04-08 21:34:36 +0100

    env

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,7 +5,7 @@ const tourRouter = express.Router();
 tourRouter.param("id", tourController.checkID);
 
 
-tourRouter.route("/").get(tourController.getAllTours).post(tourController.createTour);
+tourRouter.route("/").get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour);
 tourRouter.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = tourRouter;
