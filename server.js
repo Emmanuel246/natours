34a482fb4b757c0980bd6bf139a4c0f779b5b8af
Emmanuel246 <makanjuolaiseoluwa1@gmail.com>
2025-04-10 15:57:27 +0100

    Mongodb, mongoose and the connections to atlas and compass

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const app = require("./app"); 
+const app = require("./app");
 
-console.log(process.env);
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+// connecting to mmongodb atlas
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"));
+
+  const tourSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+  });
+
+  const Tour = mongoose.model("Tour", tourSchema);
+
+  // connecting to mmongodb commpass
+  // mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  //   useNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
+  //   useUnifiedTopology: true,
+  // })
+  // .then(() => console.log("DB connection successful"));
+// console.log(process.env);
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}......`);
 });
-
