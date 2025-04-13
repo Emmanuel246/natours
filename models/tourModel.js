dca38fda9148e3afbd69ea7593ba88fca40e4181
Emmanuel246 <makanjuolaiseoluwa1@gmail.com>
2025-04-13 17:45:02 +0100

    Middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true,
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, "A tour must have a duration"],
@@ -56,8 +58,49 @@ const tourSchema = new mongoose.Schema({
     select: false,
   },
   startDates: [Date],
-});
+  secretTour: {
+    type: Boolean,
+    default: false,
+  },
+},{
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+}
+);
 
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
+}
+);
+
+// Document Middlware: runs before .save() and .create() 
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+}
+);
+
+// Query Middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+}
+);
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+}
+);
+
+// Aggregation Middleware
+tourSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+}
+);
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
