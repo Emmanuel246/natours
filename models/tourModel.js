3d72a0f2c1b89ec1bcea888b7c4686c6b43e6d3d
Emmanuel246 <makanjuolaiseoluwa1@gmail.com>
2025-04-13 17:53:22 +0100

    inbuilt-validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
     trim: true,
+    maxLength: [40, "A tour name must have less or equal than 40 characters"],
+    minLength: [10, "A tour name must have more or equal than 10 characters"],
   },
   slug: String,
   duration: {
@@ -20,10 +22,10 @@ const tourSchema = new mongoose.Schema({
   difficulty: {
     type: String,
     required: [true, "A tour must have a difficulty"],
-    //   enum: {
-    //     values: ["easy", "medium", "difficult"],
-    //     message: "Difficulty is either: easy, medium, difficult",
-    //   },
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium, difficult",
+      },
   },
   price: {
     type: Number,
@@ -32,6 +34,8 @@ const tourSchema = new mongoose.Schema({
   ratingsAverage: {
     type: Number,
     default: 4.5,
+    min: [1, "Rating must be above 1.0"],
+    max: [5, "Rating must be below 5.0"],
   },
   ratingsQuantity: {
     type: Number,
