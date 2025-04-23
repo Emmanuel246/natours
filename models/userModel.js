729e33d31d6de17720b927f2caabd8e61a420449
Emmanuel246 <makanjuolaiseoluwa1@gmail.com>
2025-04-23 10:58:02 +0100

    reset-token and cookie

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const { type } = require("os");
 
 
 const userSchema = new mongoose.Schema({
@@ -19,6 +21,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "default.jpg",
     },
+    role : {
+        type: String,
+        enum: ["user", "guide", "lead-guide", "admin"],
+        default: "user",
+    },
     password: {
         type: String,
         required: [true, "A user must have a password"],
@@ -36,6 +43,15 @@ const userSchema = new mongoose.Schema({
             message: "Passwords are not the same!",
         },
     },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false,
+    },
+
 
 
 });
@@ -52,6 +68,20 @@ userSchema.pre("save", async function (next) {
     next();
 });
 
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password') || this.isNew) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+    next();
+});
+
+userSchema.pre(/^find/, function (next) {
+    // this points to current query
+    this.find({ active: { $ne: false } });
+    next();
+}
+);
+
 userSchema.methods.correctPassword = async function (
     candidatePassword,
     userPassword
@@ -59,6 +89,35 @@ userSchema.methods.correctPassword = async function (
     return await bcrypt.compare(candidatePassword, userPassword);
 };
 
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+    if (this.passwordChangedAt) {
+        const changedTimestamp = parseInt(
+            this.passwordChangedAt.getTime() / 1000,
+            10
+        );
+
+        return JWTTimestamp < changedTimestamp; // 1000 < 200
+    }
+
+    // False means NOT changed
+    return false;
+}
+
+
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -21,6 +21,9 @@ tourRouter
   .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect, 
+    authController.restrictTo('admin', 'lead-guide'), 
+    tourController.deleteTour);
 
 module.exports = tourRouter;
